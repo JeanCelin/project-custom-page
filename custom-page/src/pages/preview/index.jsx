d209d62a9e667fdf14a2ab307978853a687f9eee
Jean Celin <jean_estudos@hotmail.com>
2024-12-28 09:55:03 -0300

    small css changes

--- custom-page/src/pages/preview/index.jsx
+++ custom-page/src/pages/preview/index.jsx
@@ -4,6 +4,7 @@ import FooterDefault from "@/components/Footers/FooterDefault/FooterDefault";
 
 import styles from "@/styles/pages/Preview.module.css";
 import Link from "next/link";
+import Navbar1 from "@/components/Navbars/Navbar1/Navbar1";
 import Navbar2 from "@/components/Navbars/Navbar2/Navbar2";
 import Navbar3 from "@/components/Navbars/Navbar3/Navbar3";
 import Main1 from "@/components/Mains/Main1/Main1";
@@ -14,8 +15,8 @@ export default function PreviewPage() {
       <h1 className={styles.preview__title}>Preview Your Page</h1>
       <div className={styles.preview__layout}>
         <section className={styles.preview__page}>
-          <Navbar2 />
-          <Main1 />
+          <NavbarDefault />
+          <MainDefault />
         </section>
       </div>
 
